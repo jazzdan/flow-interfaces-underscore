857bab90954394446f3cfe1d13214c6649a7eb8a
Michael Robinson <michael.robinson@kiwirail.co.nz>
2014-12-19 08:52:40 +1300

    Merged iteratee with _ because autoloading order

--- interfaces/_.js
+++ interfaces/_.js
@@ -1,15 +1,51 @@
-declare class Underscore {
-   each(list: Array | Object, iteratee: iteratee.object | iteratee.array): Array | Object;
-   each(list: Array | Object, iteratee: iteratee.object | iteratee.array, context: Object): Array | Object;
-   filter(list: Array, predicate: Function): Array;
-   filter(list: Array, predicate: Function, contect: Object): Array;
+declare class _UnderscoreCallbacks_ {
+   iterateeArray(element: any): void;
+   iterateeArray(element: any, index: number): void;
+   iterateeArray(element: any, index: number, list: Array): void;
+   iterateeObject(value: any): void;
+   iterateeObject(value: any, key: string | number): void;
+   iterateeObject(value: any, key: string | number, list: Object): void;
+   iterateeReduceArray(memo: any, value: any, index: number): Array;
+   iterateeReduceArray(memo: any, value: any, key: string | number): Array;
+   predicate(value: any): boolean;
+}
+
+declare var _cb_: _UnderscoreCallbacks_;
+
+declare class _UnderscoreChain_ {
+   each(iteratee: _cb_.iterateeObject | _cb_.iterateeArray): Array | Object;
+   each(iteratee: _cb_.iterateeObject | _cb_.iterateeArray, context: Object): Array | Object;
+   filter(predicate: _cb_.predicate): Array;
+   filter(predicate: _cb_.predicate, contect: Object): Array;
+   findWhere<T>(properties: {}): T;
+   isArray(): boolean;
+   isEmpty(): boolean;
+   isObject(): boolean;
+   map(iteratee: _cb_.iterateeObject | _cb_.iterateeArray): Array | Object;
+   map(iteratee: _cb_.iterateeObject | _cb_.iterateeArray, context: Object): Array | Object;
+   omit(keys: string | Array<string> | Function): Object;
+   chain(): _UnderscoreChain_;
+   reduce(iteratee: _cb_.iterateeReduceArray): string | number;
+   reduce(iteratee: _cb_.iterateeReduceArray, memo: Array): string | number;
+   reduce(iteratee: _cb_.iterateeReduceArray, memo: Array, context: Object): string | number;
+}
+
+declare class _Underscore_ {
+   each(list: Array | Object, iteratee: _cb_.iterateeObject | _cb_.iterateeArray): Array | Object;
+   each(list: Array | Object, iteratee: _cb_.iterateeObject | _cb_.iterateeArray, context: Object): Array | Object;
+   filter(list: Array, predicate: _cb_.predicate): Array;
+   filter(list: Array, predicate: _cb_.predicate, contect: Object): Array;
    findWhere<T>(list: Array<T>, properties: {}): T;
    isArray(object: any): boolean;
    isEmpty(object: Object): boolean;
    isObject(object: any): boolean;
-   map(list: Array | Object, iteratee: iteratee.object | iteratee.array): Array | Object;
-   map(list: Array | Object, iteratee: iteratee.object | iteratee.array, context: Object): Array | Object;
+   map(list: Array | Object, iteratee: _cb_.iterateeObject | _cb_.iterateeArray): Array | Object;
+   map(list: Array | Object, iteratee: _cb_.iterateeObject | _cb_.iterateeArray, context: Object): Array | Object;
    omit(object: Object, keys: string | Array<string> | Function): Object;
+   chain(object: Object): _UnderscoreChain_;
+   reduce(list: Array, iteratee: _cb_.iterateeReduceArray): string | number;
+   reduce(list: Array, iteratee: _cb_.iterateeReduceArray, memo: Array): string | number;
+   reduce(list: Array, iteratee: _cb_.iterateeReduceArray, memo: Array, context: Object): string | number;
 }
 
-declare var _: Underscore;
+declare var _: _Underscore_;
