0cf0c99ef9d92ecadf76bc03da8cdbf69b490a5b
Michael Robinson <michael.robinson@kiwirail.co.nz>
2014-12-18 17:15:19 +1300

    Updated _ interface

--- _.js
+++ _.js
@@ -1,5 +1,15 @@
 declare class Underscore {
-
+   each(list: Array | Object, iteratee: iteratee.object | iteratee.array): Array | Object;
+   each(list: Array | Object, iteratee: iteratee.object | iteratee.array, context: Object): Array | Object;
+   filter(list: Array, predicate: Function): Array;
+   filter(list: Array, predicate: Function, contect: Object): Array;
+   findWhere<T>(list: Array<T>, properties: {}): T;
+   isArray(object: any): boolean;
+   isEmpty(object: Object): boolean;
+   isObject(object: any): boolean;
+   map(list: Array | Object, iteratee: iteratee.object | iteratee.array): Array | Object;
+   map(list: Array | Object, iteratee: iteratee.object | iteratee.array, context: Object): Array | Object;
+   omit(object: Object, keys: string | Array<string> | Function): Object;
 }
 
 declare var _: Underscore;
